533bda2f7ec02e824cfc33aeb5904523f5efcae6
dpedagada <64825894+dpedagada@users.noreply.github.com>
2020-05-05 13:06:17 +0530

    updated

--- com/TeamAveva/Test/File1.java
+++ com/TeamAveva/Test/File1.java
@@ -17,10 +17,14 @@ public class File1 {
 		int a = 20;
 		int b = 40;
 		int c = 0;
+		int d =0;
 		
-		c = a - b;
+		c = a + b;
+		d = c + b
 		
 		System.out.println("The sum is: "+c); 
+				System.out.println("The sum is: "+d); 
+
 		
 	}
 
