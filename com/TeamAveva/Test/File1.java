18c93d15d925db9d80f70b8b500c07a4822a60ab
sheshanandan-sharechat <sheshanandan.k@qualitrix.com>
2020-05-05 12:06:01 +0530

    project added with 2 files

---
+++ com/TeamAveva/Test/File1.java
@@ -0,0 +1,27 @@
+package com.TeamAveva.Test;
+
+public class File1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str = "This is the first line of code in File1 for Team Aveva";
+		
+		System.out.println(str); 
+		
+		addNum();
+
+	}
+	
+	public static void addNum() {
+		int a = 20;
+		int b = 30;
+		int c = 0;
+		
+		c = a + b;
+		
+		System.out.println("The sum is: "+c); 
+		
+	}
+
+}
